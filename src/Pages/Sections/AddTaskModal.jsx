8de228baafb4406d6893af4c44810ff066dc1ea4
User <nahidbinwadood@gmail.com>
2024-10-27 22:13:38 +0600

    feat: created task and editable

--- src/Pages/Sections/AddTaskModal.jsx
+++ src/Pages/Sections/AddTaskModal.jsx
@@ -1,10 +1,28 @@
-const AddTaskModal = () => {
+import toast from 'react-hot-toast';
+
+const AddTaskModal = ({ handleAddNewTask, updatedTask }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const title = form.title.value;
+    const description = form.description.value;
+    const tags = form.tags.value.split(',');
+    const priority = form.priority.value;
+    const task = { title, description, tags, priority };
+    if (task) {
+      toast.success('Task added successfully');
+    }
+    handleAddNewTask(task);
+  };
   return (
     <>
       <div className="w-full h-full bg-black/20 z-10 fixed inset-0 backdrop-blur"></div>
-      <form className="fixed top-[55%] left-1/2 z-20 w-full max-w-[740px] transform -translate-x-1/2 -translate-y-1/2 rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:p-11">
+      <form
+        onSubmit={handleSubmit}
+        className="fixed top-[55%] left-1/2 z-20 w-full max-w-[740px] transform -translate-x-1/2 -translate-y-1/2 rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:p-11"
+      >
         <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
-          Add New Task
+          {updatedTask ? 'Update Task' : 'Add New Task'}
         </h2>
 
         <div className="space-y-9 text-white lg:space-y-10">
@@ -15,6 +33,7 @@ const AddTaskModal = () => {
               type="text"
               name="title"
               id="title"
+              defaultValue={updatedTask ? updatedTask?.title : ''}
               required
             />
           </div>
@@ -26,6 +45,7 @@ const AddTaskModal = () => {
               type="text"
               name="description"
               id="description"
+              defaultValue={updatedTask ? updatedTask?.description : ''}
               required
             ></textarea>
           </div>
@@ -34,6 +54,7 @@ const AddTaskModal = () => {
             <div className="space-y-2 lg:space-y-3">
               <label htmlFor="tags">Tags</label>
               <input
+                defaultValue={updatedTask ? updatedTask?.tags : ''}
                 className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
                 type="text"
                 name="tags"
@@ -45,15 +66,16 @@ const AddTaskModal = () => {
             <div className="space-y-2 lg:space-y-3">
               <label htmlFor="priority">Priority</label>
               <select
+                defaultValue={updatedTask ? updatedTask?.priority : ''}
                 className="block w-full cursor-pointer rounded-md bg-[#2D323F] px-3 py-2.5"
                 name="priority"
                 id="priority"
                 required
               >
                 <option value="">Select Priority</option>
-                <option value="low">Low</option>
-                <option value="medium">Medium</option>
-                <option value="high">High</option>
+                <option value="Low">Low</option>
+                <option value="Medium">Medium</option>
+                <option value="High">High</option>
               </select>
             </div>
           </div>
@@ -61,11 +83,10 @@ const AddTaskModal = () => {
 
         <div className="mt-16 flex justify-center lg:mt-20">
           <button
-            onClick={'ez'}
             type="submit"
             className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
           >
-            Create new Task
+            {updatedTask ? "Save" : "Create new Task"}
           </button>
         </div>
       </form>
--- src/Pages/Sections/TaskBoard.jsx
+++ src/Pages/Sections/TaskBoard.jsx
@@ -5,14 +5,38 @@ import TaskList from './../../Components/TaskList';
 import AddTaskModal from './AddTaskModal';
 
 const TaskBoard = () => {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: 'Integration API',
+    description:
+      'Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.',
+    tags: ['web', 'react', 'js'],
+    priority: 'High',
+    isFavorite: false,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
   const [showModal, setShowModal] = useState(false);
+  const [updatedTask, setUpdatedTask] = useState();
+
+  //functions::
   const handleShowModal = () => {
     setShowModal(true);
     document.body.style.overflow = 'hidden';
   };
+
+  const handleAddNewTask = (task) => {
+    setTasks([...tasks, task]);
+    setShowModal(false);
+    document.body.style.overflow = 'auto';
+  };
+
+  const handleUpdateTask = (task) => {
+    setUpdatedTask(task);
+    setShowModal(true);
+  };
   return (
     <section className="mb-20" id="tasks">
-      {showModal && <AddTaskModal />}
+      {showModal && <AddTaskModal handleAddNewTask={handleAddNewTask} updatedTask={updatedTask} />}
 
       <div className="container">
         <div className="p-2 flex justify-end">
@@ -21,7 +45,7 @@ const TaskBoard = () => {
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskActions modal={handleShowModal} />
-          <TaskList />
+          <TaskList tasks={tasks} handleUpdateTask={handleUpdateTask} />
         </div>
       </div>
     </section>
